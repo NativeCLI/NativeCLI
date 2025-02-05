ed3254551eafc57924d5e1909cfad7358c5d395f
Pete Bishop <peter.bishop@evie.software>
2025-02-05 11:10:54 +0000

    Update namespace

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Petebishwhip\NativePhpCli\Command;
+namespace NativeCLI\Command;
 
 use Illuminate\Filesystem\Filesystem;
-use Petebishwhip\NativePhpCli\Composer;
-use Petebishwhip\NativePhpCli\Version;
+use NativeCLI\Composer;
+use NativeCLI\Version;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
