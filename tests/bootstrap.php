a5bddd4d2480e43251cc33557cba0c06a84c7a88
Pete Bishop <peter.bishop@evie.software>
2025-02-11 13:26:19 +0000

    Setup composer for test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,8 @@ define('TESTS_ROOT', __DIR__);
 define('TESTS_DATA_DIR', TESTS_ROOT . '/data');
 define('ROOT_DIR', dirname(TESTS_ROOT));
 define('SRC_DIR', ROOT_DIR . '/src');
+
+// If defined that we're in a GH action, create a composer file
+if (getenv('GITHUB_ACTIONS')) {
+    \Symfony\Component\Process\Process::fromShellCommandline('composer global require nativecli/nativecli --no-interaction')->mustRun();
+}
