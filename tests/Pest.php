6a4691553cd4a0139a8cbb3ecdcdad8fbc8e3e64
Pete Bishop <9081809+PeteBishwhip@users.noreply.github.com>
2025-11-05 14:59:00 +0000

    Make calldir in tests the root

--- tests/Pest.php
+++ tests/Pest.php
@@ -16,7 +16,7 @@ define('SRC_DIR', ROOT_DIR . '/src');
 define('TESTS_TEMP_DIR', TESTS_ROOT . '/data/tmp');
 
 if (!defined('NATIVECLI_CALL_DIRECTORY')) {
-    define('NATIVECLI_CALL_DIRECTORY', getcwd() ?: TESTS_ROOT);
+    define('NATIVECLI_CALL_DIRECTORY', getcwd() ?: TESTS_ROOT . '../');
 }
 
 // If defined that we're in a GH action, create a composer file
